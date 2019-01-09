1f386796dd74494c2f64cfbf66c53629a4202f66
masa.nosuke <ezk04056@yahoo.co.jp>
2019-01-09 00:50:13 +0000

    top page

---
+++ database/migrations/2019_01_08_152125_create_notebooks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotebooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notebooks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('display_number');
+            $table->string('title');
+            $table->string('point');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notebooks');
+    }
+}
